1dfa2a632a7f2f3b8ca494d3922e9789f9b5d9ed
Kennard Sugirotok <kennardfx@gmail.com>
2023-12-02 20:55:36 +0700

    feat: added job list in job list page

--- src/app/(pages)/jobs/page.tsx
+++ src/app/(pages)/jobs/page.tsx
@@ -1,7 +1,11 @@
+import JobList from "@/components/jobs/JobList";
+import { dummyJobs } from "@/data/dummy/jobs";
+
 const JobListPage = () => {
     return ( 
         <>
-            <h1>Job List</h1>
+            <h1 className="text-4xl font-bold leading-[80px]">Job List</h1>
+            <JobList jobs={dummyJobs} />
         </>
      );
 }
