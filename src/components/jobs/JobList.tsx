abc2dde3f1a9f69f7141e5103016a629d854e18a
FX Kennard S <91940623+kennn21@users.noreply.github.com>
2023-12-03 15:09:09 +0700

    22 add link to logo (#23)
    * feat: job list now takes real data from db
    
    * fix: added postinstall prisma generate for deploying in vercel
    
    * feat: added notification when no jobs exist
    
    * feat: installed prisma client
    
    * feat: added link to site logo

--- src/components/jobs/JobList.tsx
+++ src/components/jobs/JobList.tsx
@@ -5,10 +5,13 @@ const JobList = ({ jobs }: {jobs: Job[]}) => {
     return ( 
             <div className="justify-center w-full">
                 <div className="flex flex-row w-full flex-wrap justify-between gap-2">
-                    {
+                    
+                    { jobs ?
                         jobs.map((job, index)=>(
                             <JobListItem key={`job${index}`} job={job}/>
                         ))
+                        :
+                        <h1>No vacancies at the moment. Keep sharpening your skills for upcoming job openings!</h1>
                     }
                 </div>
             </div>
--- src/components/shared_/site-logo.tsx
+++ src/components/shared_/site-logo.tsx
@@ -1,12 +1,16 @@
 import { cn } from "@/lib/utils";
 import { SquareUserRound } from "lucide-react";
+import Link from "next/link";
 
 const SiteLogo = () => {
     return ( 
-        <div className="flex flex-nowrap gap-1 items-center">
-            <SquareUserRound className="light:text-neutral-800 dark:text-gray-100"/>
-            <h1 className={cn([, "font-extrabold text-xl light:text-neutral-800 dark:text-gray-100"])}>RECRUIT<span className=" text-purple-600">AI</span>R</h1>
-        </div>
+        <Link href={"/"}>
+            <div className="flex flex-nowrap gap-1 items-center">
+                <SquareUserRound className="light:text-neutral-800 dark:text-gray-100"/>
+                <h1 className={cn([, "font-extrabold text-xl light:text-neutral-800 dark:text-gray-100"])}>RECRUIT<span className=" text-purple-600">AI</span>R</h1>
+            </div>
+        </Link>
+
      );
 }
  
