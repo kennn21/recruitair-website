b29735e5fe8c97ed5ba573193de37dc244c2cc76
Kennard Sugirotok <kennardfx@gmail.com>
2023-12-17 19:07:16 +0700

    feat: connected job application to job list; fix: now job application works; fix: user now cannot apply to the same job twice

--- src/app/(pages)/jobs/[id]/application/page.tsx
+++ src/app/(pages)/jobs/[id]/application/page.tsx
@@ -1,11 +1,17 @@
 import JobForm from "@/components/jobs/JobForm";
 
-const ApplicationPage = () => {
+type ApplicationPageProps = {
+    params: {
+        id: string
+    };
+};
+const ApplicationPage = ({ params }: ApplicationPageProps) => {
+    const jobId = params.id;
 
     return ( 
         <>
             <h1 className="text-4xl font-bold leading-[80px] mb-10 text-center">Form Job Application</h1>
-            <JobForm jobId={0}/>
+            <JobForm jobId={Number(jobId)}/>
         </>
      );
 }
--- src/types/schema/createApplication.ts
+++ src/types/schema/createApplication.ts
@@ -1,18 +1,6 @@
 import * as z from "zod"
 
 export const createApplicationSchema = z.object({
-    username: z.string({
-      required_error: "Username is required.",
-    }),
-    email: z.string({
-      required_error: "Email is required.",
-    }),
-    phoneNumber: z.string({
-      required_error: "Phone Number is required.",
-    }),
-    dob: z.date({
-      required_error: "A date of birth is required.",
-    }),
     workExperiences: z.coerce.number({
           invalid_type_error: "Work Experiences must be a number.",
           required_error: "work Experience is required.",
