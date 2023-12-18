6a3699951d13ecfd417bf4ae25d21b723630bd23
Kennard Sugirotok <kennardfx@gmail.com>
2023-12-18 23:47:31 +0700

    feat: finished job detail page

--- src/components/jobs/JobDetail.tsx
+++ src/components/jobs/JobDetail.tsx
@@ -2,27 +2,44 @@ import { Job } from "@prisma/client";
 import { Button } from "../ui/button";
 import { PinTopIcon } from "@radix-ui/react-icons";
 
+type JobDetailProps = {
+  job: {  
+    id: number;  
+    title: string;  
+    imageUrl: string;  
+    createdAt: Date;  
+    updatedAt: Date;  
+    startDate: Date;  
+    endDate: Date;  
+    createdBy: string;  
+    updatedBy: string;  
+    description: string;  
+    requirements: string;  
+    location: string;  
+    salary: number;  
+  } | null;
+}
 
-const JobDetail = ({job}:{job: Job}) => {
+const JobDetail = ({job}:JobDetailProps) => {
     return(
         <>
           <header className="w-full">
-            <h1>{job.title}</h1>
+            <h1>{job?.title}</h1>
             <div id='grid' className="flex justify-between w-fit">
               <div id="job-location">
                 <PinTopIcon/>
-                <span>{job.location}</span>
+                <span>{job?.location}</span>
               </div>
               <div id="job-req-experience">
-                {job.requirements}
+                {job?.requirements}
               </div>
             </div>
             <section id="content">
               <h2>Job Description</h2>
-                {job.description}
+                {job?.description}
               <br/>
               <h2>Requirements</h2>
-              <p>{job.requirements}</p>
+              <p>{job?.requirements}</p>
             </section>
           </header>
         </>
--- src/components/jobs/JobList.tsx
+++ src/components/jobs/JobList.tsx
@@ -6,14 +6,16 @@ const JobList = ({ jobs }: {jobs: Job[]}) => {
     return ( 
             <div className="justify-center w-full">
                 <div className="flex flex-row w-full flex-wrap justify-center gap-2 items-center min-h-[50vh]">
-                    
-                    { jobs.length > 0 ?
+
+                    {
+                        jobs.length > 0 ?
                         jobs.map((job, index)=>(
-                            <Link href={`/jobs/${job.id}`}><JobListItem key={`job${index}`} job={job}/></Link>
+                            <Link key={`job${index}`} href={`/jobs/${job.id}`}><JobListItem job={job}/></Link>
                         ))
                         :
                         <h1>No vacancies at the moment. Keep sharpening your skills for upcoming job openings!</h1>
                     }
+
                 </div>
             </div>
      );
